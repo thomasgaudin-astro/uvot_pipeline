f227a84a5742a2b4065e367ff9946701eb44b517
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2025-06-11 16:16:48 -0400

    Allowed output of errors from uvotsource

--- uvot_pipeline.py
+++ uvot_pipeline.py
@@ -140,7 +140,7 @@ def run_uvotunicorr(uvotunicorr_command):
     )
 
     # print("STDOUT:\n", result.stdout)
-    print("STDERR:\n", result.stderr)
+    # print("STDERR:\n", result.stderr)
 
     return result.stdout
 
@@ -183,7 +183,7 @@ def run_uvotsource(uvotsource_command):
     )
 
     # print("STDOUT:\n", result.stdout)
-    # print("STDERR:\n", result.stderr)
+    print("STDERR:\n", result.stderr)
 
     return result.stdout
 
