3ad53a0185938cb14b930f9f47df8ab54ffdca70
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2025-08-08 13:34:49 -0400

    Testing my weird errors

--- plot_mw_lcs.py
+++ plot_mw_lcs.py
@@ -41,6 +41,9 @@ while prompt_ogle == True:
 
     ogle = input(f'Is there an OGLE IV Light Curve associated with this UV source? [Y/N] ')
 
+    print(ogle)
+    print(type(ogle))
+
     if ogle.upper == "Y":
 
         ogle_name = input("Please provide the OGLE IV name of the source: ")
