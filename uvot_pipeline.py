ca6dae2d73387bc85b521ccf81b915818bc4562a
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2025-09-12 16:58:46 -0400

    Fixed bug where the region file generator would break for soources where no stars were detected in the image.

--- uvot_pipeline.py
+++ uvot_pipeline.py
@@ -629,32 +629,33 @@ def write_source_reg_files(tile_name, obsid, source_name, source_ra, source_dec)
     #loop through all the stars in detect.fits
     #find the closest one to coordinate positions of source
     #use those coords to write a new .reg file out in the obs filder
-    for ind in detected_frame.index:
+    if len(detected_frame.index) >= 1:
+        for ind in detected_frame.index:
+            
+            #generate a SkyCoord object for each star
+            ra = detected_frame.loc[ind, 'RA']
+            dec = detected_frame.loc[ind, 'DEC']
+            
+            star_coords = SkyCoord(ra, dec, unit='deg', frame='fk5')
+
+            #calculate separation to source and append to dataframe
+            sep = star_coords.separation(source_coords).to(u.arcsecond)
+            detected_frame.loc[ind, 'SEP'] = sep
+
+        #look for star with min separation and grab coordinates of that star
+        min_sep = detected_frame['SEP'].idxmin()
+
+        min_ra = detected_frame.loc[min_sep, 'RA']
+        min_dec = detected_frame.loc[min_sep, 'DEC']
+
+        #check to see how far away the nearest star is before writing a region file
+        #if distance is > 5 arcseconds, no new region file is created.
+        if detected_frame.loc[min_sep, 'SEP'] <= (10 * u.arcsecond):
+            #generate new region text and write out file
+            new_reg_text = f'# Region file format: DS9 version 4.1\nfk5\ncircle({min_ra},{min_dec},5.000")'
         
-        #generate a SkyCoord object for each star
-        ra = detected_frame.loc[ind, 'RA']
-        dec = detected_frame.loc[ind, 'DEC']
-        
-        star_coords = SkyCoord(ra, dec, unit='deg', frame='fk5')
-
-        #calculate separation to source and append to dataframe
-        sep = star_coords.separation(source_coords).to(u.arcsecond)
-        detected_frame.loc[ind, 'SEP'] = sep
-
-    #look for star with min separation and grab coordinates of that star
-    min_sep = detected_frame['SEP'].idxmin()
-
-    min_ra = detected_frame.loc[min_sep, 'RA']
-    min_dec = detected_frame.loc[min_sep, 'DEC']
-
-    #check to see how far away the nearest star is before writing a region file
-    #if distance is > 5 arcseconds, no new region file is created.
-    if detected_frame.loc[min_sep, 'SEP'] <= (10 * u.arcsecond):
-        #generate new region text and write out file
-        new_reg_text = f'# Region file format: DS9 version 4.1\nfk5\ncircle({min_ra},{min_dec},5.000")'
-    
-        with open(reg_filename, mode='w', encoding='utf-8') as regfile:
-            regfile.write(new_reg_text)
+            with open(reg_filename, mode='w', encoding='utf-8') as regfile:
+                regfile.write(new_reg_text)
 
 def download_ogle_data(ogle_name, source_name):
 
