5f05700d650bb74302ce103cb15df8c9b692eb51
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2026-03-25 14:52:22 -0400

    First test of batch photometry script.

--- batch_photometry.py
+++ batch_photometry.py
@@ -9,3 +9,18 @@ args = parser.parse_args()
 
 targets = pd.read_csv(args.filename, header=None, names=['ID', 'UVOT RA', 'UVOT Dec', 'S-CUBED Tile', 'Cleaned?', 'Problems?'], sep=r'\s+')
 
+print('Starting batch processing photometry of S-CUBED sources.\n')
+print(f'Producing light curves for {len(targets.index)} sources.')
+
+for ind, target in enumerate(targets.index):
+
+    targname = targets.loc[ind, 'ID']
+    targ_ra = target[ind, 'UVOT RA']
+    targ_dec = target[ind, 'UVOT DEC']
+
+    print(f'Begin photometry for Source {targname}.')
+
+    subprocess.run(['python', 
+                    f'./run_uvot_photometry.py {targname} {targname}_source.reg {targname}_bkg.reg {targ_ra} {targ_dec}'])
+
+print('Batch processing complete. Check UVOT Outputs folder for completed light curves.')
