230df7dc019d16f01cb19f518d116dfe906bcbdc
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2026-04-01 10:23:12 -0400

    Fixed typo

--- run_uvot_photometry.py
+++ run_uvot_photometry.py
@@ -188,7 +188,7 @@ else:
     verbose = False
 
 for obsid in tqdm(all_target_filepaths):
-    up.single_uvotsource(closest_tile, obsid, args.source_name, args.source_reg, arg.sbkg_reg, verbose=verbose)
+    up.single_uvotsource(closest_tile, obsid, args.source_name, args.source_reg, args.bkg_reg, verbose=verbose)
 
 print("Aperture photometry complete.\n")
 
