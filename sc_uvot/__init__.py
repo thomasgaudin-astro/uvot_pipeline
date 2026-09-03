d20d81c3036820daf6cadf062a50bf7e4a19e6ed
thomasgaudin <78448417+thomasgaudin-astro@users.noreply.github.com>
2026-09-02 23:19:26 -0400

    Added import statements for AspectCorrections, RemoveSmeared, BackgroundGenerator, and WhichTIle

--- sc_uvot/__init__.py
+++ sc_uvot/__init__.py
@@ -2,10 +2,16 @@
 from .DownloadUVOT import DownloadUVOT
 from .FTOOLSCommands import FTOOLSCommands
 from .RemoveSmeared import RemoveSmeared
+from .AspectCorrections import AspectCorrections
+from .BackgroundGenerator import BackgroundGenerator
+from .WhichTile import WhichTile
 
 #import all
 __all__ = [
     'DownloadUVOT', 
     'FTOOLSCommands',
-    'RemoveSmeared'
+    'RemoveSmeared',
+    'AspectCorrections',
+    'BackgroundGenerator',
+    'WhichTile'
 ]
